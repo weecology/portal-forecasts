95c30c785994a7e115acecddb94fb274a0fa71aa
henrykironde <henrykironde@gmail.com>
2025-09-24 23:22:44 -0400

    Download forecasts from Zenodo before running tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,8 @@
 library(testthat)
 library(portalcasting)
+
+# Download forecasts from Zenodo before running tests
+source("../download_zenodo_forecasts.R")
+download_zenodo_forecasts()
+
 test_dir("tests/testthat", reporter = c("check"))
